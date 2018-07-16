4d8271f57f766cb4f4e27e40e04c6bea31c4b2ee
Kangbo Lu <redstonegranola@gmail.com>
2018-07-16 20:47:11 +0800

    complete priority queue class for priorityQueue.cpp

--- Data Structure/6. Queue/Priority Queue/priorityQueue.cpp
+++ Data Structure/6. Queue/Priority Queue/priorityQueue.cpp
@@ -19,26 +19,41 @@ class PriorityQueue {
 
     // insert item to the priority queue
     void insert(int item) {
-
+      // if empty, insert at index 0
+      if (nItems == 0)
+        queue[nItems++] = item;
+      else {
+        // start at the end of the queue
+        int i = nItems-1;
+        for (i; i >= 0; i--) {
+          // if new item larger than current element
+          if (item > queue[i]) 
+            queue[i+1] = queue[i]; // shift upward
+          else
+            break; // finish shifting
+        }
+        queue[i+1] = item; // insert new item
+        nItems++; // increament size counter
+      }
     }
 
     // remove minimum item
     int remove() {
-
+      return queue[--nItems];
     }
 
     // get minimum value
     int getMin() {
-
+      return queue[nItems-1];
     }
 
     // check if it is empty
     bool isEmpty() {
-
+      return nItems == 0;
     }
 
     // check if the queue is full
     bool isFull() {
-      
+      return nItems == maxSize;
     }
 }
