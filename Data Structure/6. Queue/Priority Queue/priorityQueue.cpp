cfef54268c4667e5c2cde7d5d0791af2dbe56e24
Kangbo Lu <redstonegranola@gmail.com>
2018-07-16 20:34:28 +0800

    add basic structure for priorityQueue.cpp

--- Data Structure/6. Queue/Priority Queue/priorityQueue.cpp
+++ Data Structure/6. Queue/Priority Queue/priorityQueue.cpp
@@ -1,1 +1,44 @@
 // array implementation of priority queue
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class PriorityQueue {
+  // vector in sorted order, from max at 0 to min at size-1
+  int maxSize;
+  vector<int> queue;
+  int nItems;
+  
+  public:
+    // constructor
+    PriorityQueue(int siz) {
+      maxSize = siz;
+      nItems = 0; 
+      queue.resize(maxSize);
+    }
+
+    // insert item to the priority queue
+    void insert(int item) {
+
+    }
+
+    // remove minimum item
+    int remove() {
+
+    }
+
+    // get minimum value
+    int getMin() {
+
+    }
+
+    // check if it is empty
+    bool isEmpty() {
+
+    }
+
+    // check if the queue is full
+    bool isFull() {
+      
+    }
+}
